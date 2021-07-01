9ca7ff8975731f442405dbc968f41baf9a3a6a18
Isaac Da Pena <isaacdppereiras@gmail.com>
2021-07-01 00:51:35 -0400

    Implementa el mapeador de oferta laboral a su respectivo DTO
    - Se implementa el método de conversión de la entidad de dominio a su respectivo DTO en la capa de infraestructura.
    
    Se aprecia la necesidad de la definición de la entidad de dominio  empresa que será implementada posteriormente. Se adopta el uso de identificadores y no de entidades embebidas para evitar la sobre carga en las consultas a la persistencia.

--- src/main/java/com/orangesoft/officium/empleadoApp/ofertasLaborales/infraestructura/mapeadores/MapeadorOfertaLaboralDTO.java
+++ src/main/java/com/orangesoft/officium/empleadoApp/ofertasLaborales/infraestructura/mapeadores/MapeadorOfertaLaboralDTO.java
@@ -1,7 +1,25 @@
 package com.orangesoft.officium.empleadoApp.ofertasLaborales.infraestructura.mapeadores;
 
+import com.orangesoft.officium.empleadoApp.ofertasLaborales.dominio.OfertaLaboral;
+import com.orangesoft.officium.empleadoApp.ofertasLaborales.infraestructura.dto.DtoOfertasLaboralesActivasEmpleado;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper()
-public class MapeadorOfertaLaboralDTO {
+import java.util.UUID;
+
+@Mapper(componentModel = "spring", imports = {UUID.class})
+public abstract class MapeadorOfertaLaboralDTO {
+    @Mapping(target = "uuid", expression = "java(ofertaLaboral.getIdOfertaLaboral().getIdEmpresa())")
+    @Mapping(target = "titulo", expression = "java(ofertaLaboral.getTituloOfertaLaboral().getTitulo())")
+    @Mapping(target = "fechaPublicacion", expression = "java(ofertaLaboral.getFechaPublicacionOfertaLaboral().getFechaPublicacion().toString())")
+    @Mapping(target = "cargo", expression = "java(ofertaLaboral.getCargoOfertaLaboral().getCargo())")
+    @Mapping(target = "sueldo", expression = "java(ofertaLaboral.getSueldoOfertaLaboral().getSueldo())")
+    @Mapping(target = "duracionEstimadaValor", expression = "java(ofertaLaboral.getDuracionEstimadaOfertaLaboral().getDuracionEstimada())")
+    @Mapping(target = "duracionEstimadaEscala", expression = "java(ofertaLaboral.getDuracionEstimadaOfertaLaboral().getEscalaTiempo().toString())")
+    @Mapping(target = "turnoValor", expression = "java(ofertaLaboral.getTurnoTrabajoOfertaLaboral().getTurnoTrabajo().toString())")
+    @Mapping(target = "numeroVacantes", expression = "java(ofertaLaboral.getNumeroVacantes().getNumeroVacantes())")
+    @Mapping(target = "empresaNombre", expression = "java(ofertaLaboral.getIdOfertaLaboral().getIdEmpresa())")
+    public abstract DtoOfertasLaboralesActivasEmpleado mapOfertaLaboralADto(OfertaLaboral ofertaLaboral);
+
+
 }
