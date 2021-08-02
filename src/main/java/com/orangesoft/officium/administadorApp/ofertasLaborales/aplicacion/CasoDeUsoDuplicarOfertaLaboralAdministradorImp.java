8317501a98fda7d455c271f01f3f139e09d028e4
Daniel Luis <daluisgarcia@gmail.com>
2021-08-02 02:31:35 -0400

    Agregado cambio de fecha de creación en caso de uso duplicar oferta laboral
    - Se agregó cambio de fecha de creación y modificación

--- src/main/java/com/orangesoft/officium/administadorApp/ofertasLaborales/aplicacion/CasoDeUsoDuplicarOfertaLaboralAdministradorImp.java
+++ src/main/java/com/orangesoft/officium/administadorApp/ofertasLaborales/aplicacion/CasoDeUsoDuplicarOfertaLaboralAdministradorImp.java
@@ -4,11 +4,15 @@ import com.orangesoft.officium.administadorApp.ofertasLaborales.aplicacion.entra
 import com.orangesoft.officium.administadorApp.ofertasLaborales.aplicacion.salida.PuertoCrearOfertaLaboralAdministrador;
 import com.orangesoft.officium.administadorApp.ofertasLaborales.aplicacion.salida.PuertoObtenerDetallesOfertaLaboralAdministrador;
 import com.orangesoft.officium.administadorApp.ofertasLaborales.dominio.OfertaLaboral;
+import com.orangesoft.officium.administadorApp.ofertasLaborales.dominio.valueObjects.FechaOfertaLaboral;
 import com.orangesoft.officium.administadorApp.ofertasLaborales.dominio.valueObjects.IdOfertaLaboral;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 @Component
@@ -25,6 +29,8 @@ public class CasoDeUsoDuplicarOfertaLaboralAdministradorImp implements CasoDeUso
     public void duplicarOfertaLaboral(IdOfertaLaboral idOfertaLaboral) {
         OfertaLaboral ofertaLaboral = this.puertoObtenerDetallesOfertaLaboralAdministrador.obtenerDetalles(idOfertaLaboral);
         ofertaLaboral.setIdOfertaLaboral(new IdOfertaLaboral(UUID.randomUUID()));
+        ofertaLaboral.setFechaPublicacionOfertaLaboral(new FechaOfertaLaboral(DateTimeFormatter.ofPattern("yyyy-MM-dd").withZone(ZoneId.systemDefault()).format(LocalDateTime.now())));
+        ofertaLaboral.setFechaUltimaModificacionOfertaLaboral(new FechaOfertaLaboral(DateTimeFormatter.ofPattern("yyyy-MM-dd").withZone(ZoneId.systemDefault()).format(LocalDateTime.now())));
         this.puertoCrearOfertaLaboralAdministrador.crearOfertaLaboral(ofertaLaboral);
     }
 }
